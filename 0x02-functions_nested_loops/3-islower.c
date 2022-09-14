521683779f548dcbf49d540d3e2610bf0ce7db0b
Chris <chris.nobtech@gmail.com>
2022-09-13 20:08:16 -0700

    Task-3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * _islower - tests for lowercase character
+ * @c: The character to be checked
+ *
+ * Return: 1 for lowercase character or 0 for anything else
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+
+}
