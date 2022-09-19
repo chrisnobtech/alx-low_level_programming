4b6c41ee807e6019a84594eefd19bb50660c5c4f
Chris <chris.nobtech@gmail.com>
2022-09-19 16:28:35 -0700

    Task-6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * puts2 - prints a string with a new line
+ * @str: string
+ *
+ *  Return: nothing
+ */
+
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (*(str + i) != '\0')
+	{
+		if (i % 2 == 0)
+			putchar(*(str + i));
+		i++;
+	}
+	putchar(10);
+}
