6d215c0dd72d58d5c9ac266799c3154c2c7e6570
Chris <chris.nobtech@gmail.com>
2022-09-22 01:03:31 -0700

    Task-1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,27 +1,25 @@
 #include "main"
 
 /**
- * _strncpy - C function that copies a string
- * @dest: string to be copied to
- * @src: string being copied
+ * _strncat - add src to the dest string
+ * @dest: string to be add by src
+ * @src: string be add to dest
  * @n:max nummber of byte copied
  *
  * Return: Link of dest
  */
-char *_strncpy(char *dest, char *src, int n)
-{
-	int i;
 
-	i = 0;
-	while (i < n && *(src + i))
-	{
-		*(dest + i) = *(src + i);
-		i++;
-	}
-	while (i < n)
-	{
-		*(dest + i) = '\0';
-		i++;
-	}
+char *_strncat(char *dest, char *src, int n)
+{
+
+	int index = 0, dest_len = 0;
+
+	while (dest[index++])
+		dest_len++;
+
+	for (index = 0; src[index] && index < n; index++)
+		dest[dest_len++] = src[index];
+
 	return (dest);
+
 }
