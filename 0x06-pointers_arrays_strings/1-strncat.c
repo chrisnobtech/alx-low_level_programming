3dcc4d025f086a66051dbc286c3d12b08cf90458
Chris <chris.nobtech@gmail.com>
2022-09-22 01:10:33 -0700

    Task-1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -8,18 +8,20 @@
  *
  * Return: Link of dest
  */
-
 char *_strncat(char *dest, char *src, int n)
 {
+	int i, j;
 
-	int index = 0, dest_len = 0;
-
-	while (dest[index++])
-		dest_len++;
-
-	for (index = 0; src[index] && index < n; index++)
-		dest[dest_len++] = src[index];
-
+	i = j = 0;
+	while (*(dest + i))
+		i++;
+	while (j < n && *(src + j))
+	{
+		*(dest + i) = *(src + j);
+		i++;
+		j++;
+	}
+	if (j < n)
+		*(dest + i) = *(src + j);
 	return (dest);
-
 }
