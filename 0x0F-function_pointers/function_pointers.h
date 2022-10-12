31d07c57fca7eeac99658b139fdfbb3fa93678a8
Chris <chris.nobtech@gmail.com>
2022-10-12 11:11:36 -0700

    Tasks-4

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,8 +1,11 @@
 #ifndef _HEADER_
 #define _HEADER_
+#ifndef _function_pointers_h_
+#define _function_pointers_h_
 #include <stdlib.h>
 
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
 #endif
+#endif /* _function_pointers_h_ */
